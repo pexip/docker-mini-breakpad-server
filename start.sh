297358d2cfcd8c9ccdfb124fe2ae6daf1552be4b
Geir Sporsheim <geir.sporsheim@gmail.com>
2015-08-13 19:03:46 +0200

    Download symbols from github release

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-docker run --rm -it -v `pwd`/pool:/home/nvm/mini-breakpad-server/pool -P sporsh/mini-breakpad-server
+SYMBOLS_URL=https://github.com/pexip/docker-mini-breakpad-server/releases/download/symbols/symbols.tar.gz
+# curl $SYMBOLS_URL | tar xz
+if [ -d pool ]
+    then
+        echo "Not downloading symbols: pool present"
+    else
+        echo "Downloading nw.js symbols"
+        curl -L $SYMBOLS_URL | tar xz
+fi
+
+docker build -t pexip/mini-breakpad-server docker
+docker run --rm -it -v `pwd`/pool:/home/nvm/mini-breakpad-server/pool -P pexip/mini-breakpad-server
